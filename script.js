bcd7b8ebff63f8e2a438b3b4410039e162275b4a
Bonjardim <wellingtonbonjardim7@gmail.com>
2022-06-15 20:09:27 -0300

    ESTUDOS JS - TO DO LIST

---
+++ script.js
@@ -0,0 +1,35 @@
+function drag(event) {
+    event.dataTransfer.setData("card", event.target.id)
+}
+
+function over(event) {
+    event.preventDefault()
+}
+
+function drop(event, id) {
+    event.preventDefault()
+    const target = document.getElementById(id)
+    const data = event.dataTransfer.getData("card")
+    const card = document.getElementById(data)
+    target.appendChild(card)
+    event.dataTransfer.clearData()
+}
+
+function addCard(element) {
+    const ulId = element.previousElementSibling.id
+    const text = prompt("Insira a tarefa:")
+    const board = document.getElementById(ulId)
+
+    const template = `
+        <li id="${new Date().getTime()}"  draggable="true" ondragstart="drag(event)">
+            <p>${text}</p>
+            <p class="remove" onclick="removeCard(this)">x</p>
+        </li>
+    `
+    
+    board.innerHTML = board.innerHTML + template
+}
+
+function removeCard(element) {
+    document.getElementById(element.parentElement.id).remove()
+}
